cd979e90350f0c30426ec4174eb6d62ed2999fbb
jonathans0ramirez <jonathan.ramirez@experimentality.co>
2022-01-29 18:37:41 -0500

    toggle

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.colormyviews
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var toggle = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,18 +24,15 @@ class MainActivity : AppCompatActivity() {
         val boxThreeText = findViewById<TextView>(R.id.box_three_text)
         val boxFourText = findViewById<TextView>(R.id.box_four_text)
         val boxFiveText = findViewById<TextView>(R.id.box_five_text)
+        val boxSixText = findViewById<TextView>(R.id.box_six_text)
+        val boxSevenText = findViewById<TextView>(R.id.box_seven_text)
 
         val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
-
-        val redButton = findViewById<Button>(R.id.red_button)
-        val greenButton = findViewById<Button>(R.id.green_button)
-        val yellowButton = findViewById<Button>(R.id.yellow_button)
 
         val clickableViews: List<View> =
             listOf(
                 boxOneText, boxTwoText, boxThreeText,
-                boxFourText, boxFiveText, rootConstraintLayout,
-                redButton, greenButton, yellowButton
+                boxFourText, boxFiveText, boxSixText, boxSevenText, rootConstraintLayout
             )
 
         for (item in clickableViews) {
@@ -42,21 +41,28 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun makeColored(view: View) {
-        when (view.id) {
+        toggle = if (toggle) {
             // Boxes using Color class colors for the background
-            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
-            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+            box_one_text.setBackgroundColor(Color.DKGRAY)
+            box_two_text.setBackgroundColor(Color.GRAY)
 
-            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
-            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
-            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            box_three_text.setBackgroundColor(Color.BLUE)
+            box_four_text.setBackgroundColor(Color.MAGENTA)
+            box_five_text.setBackgroundColor(Color.BLUE)
+            box_six_text.setBackgroundColor(Color.YELLOW)
+            box_seven_text.setBackgroundColor(Color.CYAN)
+            false
+        } else {
+            // Boxes using Color class colors for the background
+            box_one_text.setBackgroundColor(Color.WHITE)
+            box_two_text.setBackgroundColor(Color.WHITE)
 
-            // Boxes using custom colors for background
-            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
-            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
-            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
-
-            else -> view.setBackgroundColor(Color.LTGRAY)
+            box_three_text.setBackgroundColor(Color.WHITE)
+            box_four_text.setBackgroundColor(Color.WHITE)
+            box_five_text.setBackgroundColor(Color.WHITE)
+            box_six_text.setBackgroundColor(Color.WHITE)
+            box_seven_text.setBackgroundColor(Color.WHITE)
+            true
         }
     }
 }
